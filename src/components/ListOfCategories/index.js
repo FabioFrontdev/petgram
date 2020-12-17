654a996faf80f61e29f15885d25ad6352a26323a
Fabio Leonardo Fuentes <fabioleon.fuentes@gmail.com>
2020-12-17 02:37:36 -0500

    se agregan cambios en el front de la app, se agrega la carpeta styles para las utilidades y estilos globales de la app

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,44 @@
-/* eslint-disable react/jsx-key */
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
-import { categories } from '../../../api/db.json'
+
 import { List, Item } from './styles'
+
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    window.fetch('https://petgram.fabiofrontdev.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
+  // El segundo useEffect nos ayuda a controlar el contenido que queremos mostrar haciendo scroll pasando un alto en px determinado
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
