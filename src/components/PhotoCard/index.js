812972bfc90b18ea71b28dcabc3cafb5d9383059
Fabio Leonardo Fuentes <fabioleon.fuentes@gmail.com>
2021-01-06 23:23:23 -0500

    Se crean las carpetas container, hoc, y hooks para separar los componentes presentacionales de los contenedores, los hooks para que sean reutilizables y un componente de orden superior. Estos componente son los encargados de la lógica principal, el fetching de datos y algunas llamadas al servidor para obtener datos

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,29 +1,15 @@
-import React, { Fragment, useEffect, useState, useRef } from 'react'
+import React, { Fragment } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
-
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const ref = useRef(null)
-  const [show, setShow] = useState(false)
-
-  useEffect(function () {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(ref.current)
-    })
-  }, [ref])
+  const [show, ref] = useNearScreen()
+  const key = `like-${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={ref}>
@@ -36,8 +22,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               </ImgWrapper>
             </a>
 
-            <Button>
-              <MdFavoriteBorder size='32px' /> {likes} likes!
+            <Button onClick={() => setLiked(!liked)}>
+              <Icon size='32px' /> {likes} likes!
             </Button>
           </>
       }
