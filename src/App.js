8c34cbe15ca67d6d07b817e4b19bd00ea792c6be
Fabio Leonardo Fuentes <fabioleon.fuentes@gmail.com>
2021-01-08 23:41:59 -0500

    Se agregan dos archivos (PhotoCardWithQuery y ToggleLikeMutation) el primero para sacar la Query y poder visualizar los detalles de una PhotoCard independiente de todas las PhotoCards de la categoria, el segundo nos permite crear a traves del componente Mutation de React Apollo que el usuario pueda dar like y se aumente en el contador sin necesidad de meternos al archivo principal en el server. Por otro lado independizamos el Button de los likes en un componente aparte (FavButton) esto con el fin de ordenar el código y usar el ToggleLikeMutation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
-export const App = () => (
-  <>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={1} />
-  </>
-)
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <><ListOfCategories /><ListOfPhotoCards categoryId={1} /></>
+      }
+    </>
+  )
+}
