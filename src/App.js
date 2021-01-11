d8a35018facea3651fd3b34f58278dc2546c4caf
Fabio Leonardo Fuentes <fabioleon.fuentes@gmail.com>
2021-01-10 20:08:10 -0500

    Se agregann cambios y archivos de paginas de user y favs, se crea un hook para el formulario y sus valores, obteniendolos como siempre a travez de props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { NavBar } from './components/Nabvar'
+import { NavBar } from './components/NavBar/index'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
 import { Router } from '@reach/router'
+import Context from './Context'
 
 export const App = () => {
   return (
@@ -18,6 +22,16 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      <Context.Consumer>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router><Favs path='/favs' /><User path='/user' /></Router>
+              : <Router><NotRegisteredUser path='/favs' /><NotRegisteredUser path='/user' /></Router>
+        }
+      </Context.Consumer>
+
       <NavBar />
     </>
   )
